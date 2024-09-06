c6d00daff4525ac47af6026df11b3ab8c06e329c
Yehor <72915890+YehorMovchan@users.noreply.github.com>
2024-09-06 08:37:39 +0000

    remove lines

--- fbs/static/script.js
+++ fbs/static/script.js
@@ -3,7 +3,5 @@ function getFormattedDate(dateStr){
     let day = date.getDate() <10 ? "0"+date.getDate() : date.getDate(); 
     let month = date.getMonth()+1 <10 ? "0"+(date.getMonth()+1) : date.getMonth()+1; 
     let year = date.getFullYear();
-
-
     return `${year}-${month}-${day}`
 }
