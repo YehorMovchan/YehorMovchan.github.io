d217f3a2b6b2cca7040a910c7f7d3948e282d211
Yehor <72915890+YehorMovchan@users.noreply.github.com>
2024-09-06 08:22:04 +0000

    get was

--- fbs/attendance/kids/statistic/script.js
+++ fbs/attendance/kids/statistic/script.js
@@ -45,16 +45,6 @@ function getAttendance(id){
     })
 }
 
-function getFormattedDate(dateStr){
-    let date = new Date(dateStr)
-    let day = date.getDate() <10 ? "0"+date.getDate() : date.getDate(); 
-    let month = date.getMonth()+1 <10 ? "0"+(date.getMonth()+1) : date.getMonth()+1; 
-    let year = date.getFullYear();
-
-
-    return `${year}-${month}-${day}`
-}
-
 
 function setKidAttendance(data){
     document.getElementById("attendance").innerHTML=""
