e1af10d4429e69cd62c5cc3a6f6f65904a0b9e92
Yehor <72915890+YehorMovchan@users.noreply.github.com>
2025-03-02 19:27:27 +0200

    Delete fbs directory

--- fbs/static/script.js
+++
@@ -1,7 +0,0 @@
-function getFormattedDate(dateStr){
-    let date = new Date(dateStr);
-    let day = date.getDate() <10 ? "0"+date.getDate() : date.getDate(); 
-    let month = date.getMonth()+1 <10 ? "0"+(date.getMonth()+1) : date.getMonth()+1; 
-    let year = date.getFullYear();
-    return `${year}-${month}-${day}`
-}
